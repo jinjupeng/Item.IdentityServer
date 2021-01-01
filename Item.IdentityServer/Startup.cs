9f7ef3dbd6a7d7dac848f213030109da10148eb7
jinjupeng <im.jp@outlook.com>
2021-01-01 18:07:17 +0800

    IdentityServer简单功能实现

--- Item.IdentityServer/Startup.cs
+++ Item.IdentityServer/Startup.cs
@@ -1,31 +1,68 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace Item.IdentityServer
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
         {
             Configuration = configuration;
+            Environment = environment;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // 直接看IdentityServer4官网，讲的最清楚（https://identityserver4.readthedocs.io/en/latest/quickstarts/1_client_credentials.html）
+            //var builder = services.AddIdentityServer(options =>
+            //{
+            //    options.Events.RaiseErrorEvents = true;
+            //    options.Events.RaiseInformationEvents = true;
+            //    options.Events.RaiseFailureEvents = true;
+            //    options.Events.RaiseSuccessEvents = true;
+            //})
+            var builder = services.AddIdentityServer()
+
+            // in-memory, code config
+            .AddTestUsers(InMemoryConfig.Users().ToList())
+            .AddInMemoryApiResources(InMemoryConfig.GetApiResources())
+            .AddInMemoryClients(InMemoryConfig.GetClients())
+            .AddInMemoryApiScopes(InMemoryConfig.ApiScopes);
+
+
+            if (Environment.IsDevelopment())
+            {
+                //This is for dev only scenarios when you don’t have a certificate to use.
+                builder.AddDeveloperSigningCredential();
+            }
+            else
+            {
+                throw new Exception("need to configure key material");
+            }
+
             services.AddControllers();
+
+            //配置认证服务
+            services.AddAuthentication("Bearer").AddJwtBearer("Bearer", options =>
+            {
+                // 意思是：直接相信"http://localhost:5000"的认证，而不用再直接实现一个认证了
+                options.Authority = "http://localhost:5000";
+                options.RequireHttpsMetadata = false;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateAudience = false
+                };
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,10 +75,13 @@ namespace Item.IdentityServer
             // 添加IdentityServer中间件到Pipeline
             app.UseIdentityServer();
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
+            // 先认证
+            app.UseAuthentication();
+            // 后授权
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
